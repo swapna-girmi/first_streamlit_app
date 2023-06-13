d6c0a7cbb875d6eedbef678b862ee98d54116c2a
swapna-girmi <135969553+swapna-girmi@users.noreply.github.com>
2023-06-13 19:18:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
 streamlit.write('The user entered ', fruit_choice)
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +"jackfruit")
+
 
