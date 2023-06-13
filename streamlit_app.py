cc9e67e4b2bb69c3ef25387dbf1841792088935d
swapna-girmi <135969553+swapna-girmi@users.noreply.github.com>
2023-06-13 19:20:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,12 +43,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-# Display the table on the page.
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
-streamlit.write('The user entered ', fruit_choice)
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +"jackfruit")
 
 
