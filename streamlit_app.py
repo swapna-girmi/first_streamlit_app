51650ba77a97f353a1b0f88463a124086bcde6ec
swapna-girmi <135969553+swapna-girmi@users.noreply.github.com>
2023-06-14 19:12:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ streamlit.text(' 🥑🍞 avokado thost ')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -26,19 +26,29 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_selected )
 
 # Display the table on the page.
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.header('Fruityvice Fruit Advice!')
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+  streamlit.error("please select a fruit to get inforamtion about.")
+  else
+  fruityvice_responce = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized)
+  
+  expect URLError as e:
+    streamlit.error()
 streamlit.write('The user entered ', fruit_choice)
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +"kiwi")
+#import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
-streamlit.stop();
+
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
