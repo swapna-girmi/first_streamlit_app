300d62ca0b38afd9507d7207acc65cc36d58a1b4
swapna-girmi <135969553+swapna-girmi@users.noreply.github.com>
2023-06-15 16:07:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+my_fruit_list = my_fruit_list.set_index('Fruit')import snowflake.connector
 
 
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -36,7 +36,6 @@ try:
     fruityvice_responce = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
-  
  expect exception as e:
     print(e)
 #streamlit.write('The user entered ', fruit_choice)
