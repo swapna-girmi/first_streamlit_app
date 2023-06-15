09f80ce4c8ad55ea68fce96d4a9498aef4851df7
swapna-girmi <135969553+swapna-girmi@users.noreply.github.com>
2023-06-15 16:04:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,17 +27,18 @@ streamlit.dataframe(fruits_selected )
 
 # Display the table on the page.
 streamlit.header('Fruityvice Fruit Advice!')
-#try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-#if not fruit_choice:
-  #streamlit.error("please select a fruit to get inforamtion about.")
-  #else
-  fruityvice_responce = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  streamlit.dataframe(fruityvice_normalized)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
+    print(fruit_choice)
+    #if not fruit_choice:
+      #streamlit.error("please select a fruit to get inforamtion about.")
+      #else
+      fruityvice_responce = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
   
-  expect URLError as e:
-    streamlit.error()
+ expect exception as e:
+    print(e)
 #streamlit.write('The user entered ', fruit_choice)
 
 import requests
@@ -50,7 +51,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
-
+import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
