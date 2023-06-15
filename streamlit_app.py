a2e2a653aa8e32cc393623485047142d0587770d
swapna-girmi <135969553+swapna-girmi@users.noreply.github.com>
2023-06-15 17:43:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,25 +26,25 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_selected )
 
 
-# create a function(repeatable code block)
-def get_fruityvice_data(this_fruit_choice):
-        fruityvice_responce = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_responce.json())
-        return fruityvice_normalized
-# Display the table on the page.
-streamlit.header('Fruityvice Fruit Advice!')
-try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice:
-        streamlit.error("please select a fruit to get inforamtion about.")
-    else:
-        back_from_function = get_fruityvice_data(fruit_choice)
-        streamlit.dataframe(back_from_function)
-        #fruityvice_responce = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        #fruityvice_normalized = pandas.json_normalize(fruityvice_responce.json())
-        #streamlit.dataframe(fruityvice_normalized)
-except URLError as e:
-    streamlit.error()
+# # create a function(repeatable code block)
+# def get_fruityvice_data(this_fruit_choice):
+#         fruityvice_responce = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+#         fruityvice_normalized = pandas.json_normalize(fruityvice_responce.json())
+#         return fruityvice_normalized
+# # Display the table on the page.
+# streamlit.header('Fruityvice Fruit Advice!')
+# try:
+#     fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#     if not fruit_choice:
+#         streamlit.error("please select a fruit to get inforamtion about.")
+#     else:
+#         back_from_function = get_fruityvice_data(fruit_choice)
+#         streamlit.dataframe(back_from_function)
+#         #fruityvice_responce = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#         #fruityvice_normalized = pandas.json_normalize(fruityvice_responce.json())
+#         #streamlit.dataframe(fruityvice_normalized)
+# except URLError as e:
+#     streamlit.error()
 
 
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
@@ -57,23 +57,23 @@ except URLError as e:
 
 streamlit.header("The fruit load list contains:")
 #snowflake-related functions
-def get_fruit_load_list():
-        with my_cnx.cursor() as my_cur:
-             my_cur.execute("select * from fruit_load_list")
-             return my_cur.fetchall()
-#add a buttom to load the fruit
-if streamlit.button('Get Fruit Load List'):
-      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-      my_data_rows = get_fruit_load_list()
-      streamlit.dataframe(my_data_rows)
+# def get_fruit_load_list():
+#         with my_cnx.cursor() as my_cur:
+#              my_cur.execute("select * from fruit_load_list")
+#              return my_cur.fetchall()
+# #add a buttom to load the fruit
+# if streamlit.button('Get Fruit Load List'):
+#       my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#       my_data_rows = get_fruit_load_list()
+#       streamlit.dataframe(my_data_rows)
 
 #allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
         with my_cnx.cursor() as my_cur:
                 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
                 return "Thanks for adding " + new_fruit
-        add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-        if streamlit.button('Add a fruit to the list'):
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a fruit to the list'):
          my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
          back_from_function = insert_row_snowflake(add_my_fruit)
          streamlit.text(back_from_function)
